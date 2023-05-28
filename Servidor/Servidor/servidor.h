b16ce221acaa017d9cb997dfecd6a0c65f7b8c2a
T1ag0Card0s0 <117290011+T1ag0Card0s0@users.noreply.github.com>
2023-05-28 22:08:44 +0100

    add

---
+++ Servidor/Servidor/servidor.h
@@ -0,0 +1,110 @@
+#include <tchar.h>
+#include <math.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <io.h>
+#include <windows.h>
+#include <time.h>
+#define TAM 256
+#define TAM_BUF 10
+
+#define MAX_ROADS 8
+#define MAX_WIDTH 20
+#define MAX_CARS_PER_ROAD 8
+#define MAX_PLAYERS 2
+
+#define EXIT_EVENT _T("ExitEvent")
+#define UPDATE_EVENT _T("UpdateEvent")
+#define WRITE_SEMAPHORE _T("WriteSemaphore")
+#define READ_SEMAPHORE _T("ReadSemaphore")
+#define MUTEX_CONSUMIDOR _T("MutexConsumidor")
+#define MUTEX_SERVER _T("Server")
+#define MUTEX_ROAD _T("MutexRoad")
+#define SHARED_MEMORY_NAME _T("ShareMemory")
+
+#define KEY_SPEED TEXT("Speed")
+#define KEY_ROADS TEXT("Roads")
+
+#define KEY_DIR TEXT("Software\\TPSO2\\")
+
+#define CAR _T('C')
+#define FROG _T('F')
+
+//estrutura para o buffer circular
+typedef struct CELULA_BUFFER {
+    int id;
+    TCHAR str[100];
+}CELULA_BUFFER;
+//representa a nossa memoria partilhada
+typedef struct BUFFER_CIRCULAR {
+    int nProdutores;
+    int nConsumidores;
+    int posE; //proxima posicao de escrita  
+    int posL; //proxima posicao de leitura
+    CELULA_BUFFER buffer[TAM_BUF]; //buffer circular em si (array de estruturas)
+}BUFFER_CIRCULAR;
+
+typedef struct SHARED_BOARD {
+    DWORD dwWidth;
+    DWORD dwHeight;
+
+    TCHAR board[MAX_ROADS + 4][MAX_WIDTH];
+}SHARED_BOARD;
+
+typedef struct SHARED_MEMORY {
+    BUFFER_CIRCULAR bufferCircular;
+    SHARED_BOARD sharedBoard;
+}SHARED_MEMORY;
+//estrutura de apoio
+typedef struct SHARED_DATA {
+    SHARED_MEMORY* memPar; //ponteiro para a memoria partilhada
+    HANDLE hSemEscrita; //handle para o semaforo que controla as escritas (controla quantas posicoes estao vazias)
+    HANDLE hSemLeitura; //handle para o semaforo que controla as leituras (controla quantas posicoes estao preenchidas)
+    HANDLE hMutex;
+    HANDLE hdll;
+    int terminar; // 1 para sair, 0 em caso contrario
+    int id;
+}SHARED_DATA;
+
+typedef enum WAY { UP, DOWN, LEFT, RIGHT, STOP }WAY;
+typedef struct OBJECT {
+    DWORD dwX, dwY;
+    DWORD dwLastX, dwLastY;
+    TCHAR c;
+}OBJECT;
+typedef struct ROAD {
+    DWORD dwNumOfCars;
+    DWORD dwNumOfObjects;
+    DWORD dwSpaceBetween;
+    DWORD dwSpeed;
+    DWORD dwTimeStoped;
+
+    HANDLE hMutex;
+    HANDLE hThread;
+
+    WAY lastWay;
+    WAY way;
+
+    OBJECT cars[MAX_CARS_PER_ROAD];
+    OBJECT objects[MAX_CARS_PER_ROAD];
+}ROAD;
+
+typedef struct GAME {
+    DWORD dwLevel;
+    DWORD dwShutDown;
+    DWORD dwInitSpeed, dwInitNumOfRoads;
+    BOOL  bPaused;
+
+    HANDLE hKey;
+
+    SHARED_DATA sharedData;
+    OBJECT players[MAX_PLAYERS];
+    ROAD roads[MAX_ROADS];
+}GAME;
+
+void initRoads(ROAD* roads, DWORD dwNumOfRoads);
+void initRegestry(GAME* data);
+void moveObject(OBJECT* objData, WAY way);
+void initPlayers(OBJECT* players, DWORD dwNumRoads);
+typedef int (*PFUNC_CONS)(GAME*);
+typedef int (*PFUNC_UPDATE)(GAME*);
